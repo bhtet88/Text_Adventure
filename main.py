4b779ca593d11ab34d64b28c79eb1cc881a31a36
Brandon Htet <bhtet@berkeley.edu>
2021-01-30 21:53:52 -0800

    Fixing paragraph outputs

--- main.py
+++ main.py
@@ -3,66 +3,66 @@ from story import *
 import random
 import textwrap
 
-wrapper = textwrap.TextWrapper(width=70)
+wrapper = textwrap.TextWrapper(width=100)
 
 ### Creating player character and escaping the cell ###
 
 def escaping(player):
     """Function that is played only when the player takes any of the non-fighting situations. The player is given a few options but regardless of what they choose, they will eventually have to fight a guard and then escape their cell, where all pathways meet back up again."""
-    print(planning)
+    print(wrapper.fill(planning))
     input()
     inspection = choices(planning_choices, "What will you do?")
     print()
     if inspection == 0: #Inspecting toilet
-        print(inspect_toilet)
+        print(wrapper.fill(inspect_toilet))
         input()
         toilet_choice = choices(["Do nothing", "Clog the toilet", "Throw water on the guard as they pass by"], "What will you do?")
         print()
         if toilet_choice == 0:
             return escaping(player)
         elif toilet_choice == 1: 
-            print(toilet_clog)
+            print(wrapper.fill(toilet_clog))
         elif toilet_choice == 2:
-            print(throw_water)
+            print(wrapper.fill(throw_water))
         input()
         guard_slip = choices(["Beat the guard up", "Rip off their helmet and slam their head on the stone bench"], "What will you do?")
         print()
         if guard_slip == 0:
-            print(guard_beat)
+            print(wrapper.fill(guard_beat))
             return
         elif guard_slip == 1:
-            print(guard_smash)
+            print(wrapper.fill(guard_smash))
             return
     elif inspection == 1: #Inspecting can of food
-        print(inspect_food)
+        print(wrapper.fill(inspect_food))
         input()
         food_choice = choices(["Do nothing", "Peel off the lid and try to sharpen it with the edge of other lids"], "What will you do?")
         print()
         if food_choice == 0:
             return escaping(player)
         elif food_choice == 1:
-            print(food_lid)
+            print(wrapper.fill(food_lid))
             input()
             player.inventory_add(eval(shank))
             print()
             food_choice = choices(["Bang on the walls, acting insane", "Lie down on the bench and moan as if you were in pain", "Insult the guard when they walk past"], "Now what will you do?")
             print()
             if food_choice == 0: #Act insane
-                print(act_insane)
+                print(wrapper.fill(act_insane))
             elif food_choice == 1: #Moan
-                print(moaning)
+                print(wrapper.fill(moaning))
             elif food_choice == 2: #Insult guard
                 choices(["'You really think I'll be here any longer?'", "'Hey asshole, you enjoy being the Federation's bitch?'", "'Bet you are content with your shitty life'", "'You wear that helmet because you look like shit huh?'", "'Hey, fuck you!'"], "What will you say?")
                 print()
-                print(guard_insult)
+                print(wrapper.fill(guard_insult))
     elif inspection == 2: #Make a loud noise 
-        print(act_insane)
+        print(wrapper.fill(act_insane))
     elif inspection == 3: #Insult the guard
         choices(["'You really think I'll be here any longer?'", "'Hey asshole, you enjoy being the Federation's bitch?'", "'Bet you are content with your shitty life'", "'You wear that helmet because you look like shit huh?'", "'Hey, fuck you!'"], "What will you say?")
         print()
-        print(guard_insult)
+        print(wrapper.fill(guard_insult))
     input()
-    print(first_fight)
+    print(wrapper.fill(first_fight))
     input()
     cell = Cell()
     onward(player, cell)
@@ -74,27 +74,27 @@ player.place, generic_place.player = generic_place, player
 print()
 print(wrapper.fill(opening.format(player.name)))
 input()
-print(footsteps)
+print(wrapper.fill(footsteps))
 input()
 first_choice = choices(first_options, "What will you do?")
 print() 
 if first_choice == 0: #Player stops looking
-    print(stop_looking.format(random.choice(["male", "female"])))
+    print(wrapper.fill(stop_looking.format(random.choice(["male", "female"]))))
     input()
     escaping(player)
 elif first_choice == 1: #Player continues to look at the hallway
-    print(guard_description)
+    print(wrapper.fill(guard_description))
     input()
-    print(caught_looking.format(random.randint(10000, 99999)))
+    print(wrapper.fill(caught_looking.format(random.randint(10000, 99999))))
     input()
     first_guard = choices(caught_looking_options, "What will you do?")
     print()
     if first_guard in [0, 1, 2, 3]: #Submitting to the guard
-        print(guard_submit)
+        print(wrapper.fill(guard_submit))
         input()
         escaping(player)
     elif first_guard == 4: #Offending the guard
-        print(guard_offend)
+        print(wrapper.fill(guard_offend))
         input()
         offend_decision = choices(punish_choices, "What will you do?")
         print()
@@ -102,12 +102,12 @@ elif first_choice == 1: #Player continues to look at the hallway
             cell = Cell()
             onward(player, cell)
         elif offend_decision in [0, 1]:
-            print(guard_punish) 
+            print(wrapper.fill(guard_punish)) 
             input()
             escaping(player)
 
 input() #Getting out of the cell and facing the second guard
-print(escape)
+print(wrapper.fill(escape))
 input()
 if shank in player.inventory:
     player.inventory_remove(shank)
@@ -119,21 +119,21 @@ Prison_Guard.can_drop = True
 cell = Cell()
 onward(player, cell)
 input()
-print(spotted)
+print(wrapper.fill(spotted))
 input()
-print(pa_voice)
+print(wrapper.fill(pa_voice))
 input()
-print(alarm_sounded)
+print(wrapper.fill(alarm_sounded))
 input()
 
 ### Fighting through Lower Prison ###
 revolt = choices(["Gear yourself up and don't free the prisoners", "Free the prisoners in your wing"], "What will you do?")
 print()
 if revolt == 1:
-    print(free_wing8)
+    print(wrapper.fill(free_wing8))
     Lower_Prison.max_enemies = 2
 elif not revolt:
-    print(forget_wing8)
+    print(wrapper.fill(forget_wing8))
     player.armor += 100
 encounters = 4
 while encounters:
@@ -143,22 +143,21 @@ while encounters:
     encounters -= 1
 input()
 if revolt == 1: #Revolt
-    print(lower_section_revolt)
+    print(wrapper.fill(lower_section_revolt))
     input()
-    print(lower_section_pa)
+    print(wrapper.fill(lower_section_pa))
     input()
-    print(lower_section_ascend_revolt)
+    print(wrapper.fill(lower_section_ascend_revolt))
 elif not revolt: #Solo escape
-    print(lower_section_solo)
+    print(wrapper.fill(lower_section_solo))
     input()
-    print(lower_section_report)
+    print(wrapper.fill(lower_section_report))
     input()
-    print(lower_section_ascend_solo)
+    print(wrapper.fill(lower_section_ascend_solo))
 
 ### Level C ###
-revolt == 1
 input()
-print(levelc_intro)
+print(wrapper.fill(levelc_intro))
 if revolt == 1:
     Main_Prison_Initial.max_enemies = 2
 fights = 0
@@ -168,9 +167,9 @@ while fights:
     onward(player, place)
     fights -= 1
 input()
-print(guards_retreat.format(("You prove to be more than a match for these guards all by yourself." if not revolt else "You and the prisoners show how capable you are, overpowering the guards almost instantly."), ("prisoner is" if not revolt else "prisoners are")))
+print(wrapper.fill(guards_retreat.format(("You prove to be more than a match for these guards all by yourself." if not revolt else "You and the prisoners show how capable you are, overpowering the guards almost instantly."), ("prisoner is" if not revolt else "prisoners are"))))
 input()
-print((levelc_rest_solo if not revolt else levelc_rest_revolt))
+print(wrapper.fill(levelc_rest_solo if not revolt else levelc_rest_revolt))
 print()
 stims = 4
 while stims:
@@ -181,9 +180,9 @@ player.inventory_add(eval(pistol))
 player.armor += 100
 player.move_speed += 1
 input()
-print(levelc_reinforcements)
+print(wrapper.fill(levelc_reinforcements))
 input()
-print(reinforcement_intimidation.format(("prisoner" if not revolt else "prisoners")))
+print(wrapper.fill(reinforcement_intimidation.format(("prisoner" if not revolt else "prisoners"))))
 if revolt == 1:
     Main_Prison.max_enemies = 2
 fights = 6
@@ -194,29 +193,29 @@ while fights:
     fights -= 1
 input()
 if not revolt:
-    print(volk_intro_solo)
+    print(wrapper.fill(volk_intro_solo))
 else:
-    print(volk_intro_revolt)
+    print(wrapper.fill(volk_intro_revolt))
 input()
-print(volk_option)
+print(wrapper.fill(volk_option))
 input()
 volk_choice = choices(volk_dialogue, "What will you say?")
 print()
 if volk_choice in [0, 1]: #Surrendering to Volk gives an early end
     print("'Very well, follow me. We have plenty of training to do. Command, this is Volkov. Situation has been resolved. Prisoner is now under my authority.'")
     input()
-    print(volk_surrender)
+    print(wrapper.fill(volk_surrender))
     input()
     quit()
-print(volk_fight)
+print(wrapper.fill(volk_fight))
 input()
 onward(player, Volk_Fight())
 input()
-print(volk_death)
+print(wrapper.fill(volk_death))
 input()
-print(labs_intro)
+print(wrapper.fill(labs_intro))
 input()
-print(first_machine)
+print(wrapper.fill(first_machine))
 input()
 player.inventory_add(eval(rifle))
 player.inventory_add(eval(ap_ammo))
@@ -228,29 +227,29 @@ while events:
     onward(player, Machine_Labs())
     events -= 1
 input()
-print(GI_room)
+print(wrapper.fill(GI_room))
 input()
-print(GI_PA)
+print(wrapper.fill(GI_PA))
 input()
-print(GI_intro)
+print(wrapper.fill(GI_intro))
 input()
-print(GI_start)
+print(wrapper.fill(GI_start))
 input()
 onward(player, GI_Boss())
 input()
-print(GI_defeated)
+print(wrapper.fill(GI_defeated))
 input()
-print(AMR_discovered)
+print(wrapper.fill(AMR_discovered))
 input()
-print(AMR_file)
+print(wrapper.fill(AMR_file))
 input()
-print(AMR_take)
+print(wrapper.fill(AMR_take))
 input()
 x50 = eval(amr)
 player.weight_limit += x50.weight
 player.inventory_add(x50)
 input()
-print(level_a_intro)
+print(wrapper.fill(level_a_intro))
 input()
 saying = choices(final_choice, "What will you say?")
 print()
@@ -261,4 +260,4 @@ for x in range(2):
 input()
 onward(player, Final_Tanks())
 input()
-print(end)
+print(wrapper.fill(end))
